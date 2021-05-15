fcd54c9f64ce1697b7dda4d7a63d5778f72cf2cd
Mauricio Scheffer <mauricioscheffer@gmail.com>
2021-05-15 16:01:54 +0100

    Remove error page

--- SampleSolrApp/Controllers/HomeController.cs
+++ SampleSolrApp/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SampleSolrApp.Models;
 
 namespace SampleSolrApp.Controllers
 {
@@ -21,12 +20,6 @@ namespace SampleSolrApp.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
